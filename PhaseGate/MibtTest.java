686ea9a1660d0e3ab04400c2dd811f62e3cd61be
Babatunde <Olaleyebabatun@gmail.com>
2024-10-21 02:55:16 +0100

    changes

---
+++ PhaseGate/MibtTest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner; 
+public class MibtTest{
+
+   public static void main(String [] args){
+
+	Scanner sc = new Scanner(System.in);
+
+	System.out.print("What is your name? ");
+	String name = sc.next();
+
+	System.out.print("What is your name? ");
+
+	String [][] extrovertIntrovert = new String[5][2];
+	String [][] sensingIntuitive = new String[5][2];
+	String [][] thinkingFeeling = new String[5][2];
+	String [][] judgingPerceptive = new String[5][2];
+
+
+   }
+
+   public static String[][] questions(String [][] extrovertIntrovert, String [][] sensingIntuitive, String [][] thinkingFeeling, String [][] judgingPerceptive){
+
+	String [][] extrovertAndIntrovert = {{}, {}, {}, {}, {}};
+
+
+   }
+}
