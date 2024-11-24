678bf1c9bd3de45a5fd3c665f070723c001d08b4
Babatunde <Olaleyebabatun@gmail.com>
2024-11-24 22:58:14 +0100

    my stack

---
+++ Test/MyStackTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyStackTest {
+
+    @Test
+    public void TestThatStackIsEmpty(){
+        boolean isEmpty = true;
+        MyStack mySt = new MyStack();
+        assertTrue(mySt.isEmpty());
+    }
+
+    @Test
+    public void TestThatStackCanTakeInput(){
+        MyStack mySt = new MyStack();
+        mySt.push("Theezy");
+        mySt.push("Babatunde");
+        mySt.push("Olaleye");
+        assertEquals(3, mySt.size());
+    }
+}
---
+++ src/MyStack.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class MyStack {
+    private ArrayList<String> element;
+    private int size;
+    public MyStack(){
+        this.element = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return element.isEmpty();
+    }
+
+    public int size(int x){
+        return this.size;
+    }
+    public void push(String input){
+
+    }
+}
